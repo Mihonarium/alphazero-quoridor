16e0d82aa26782f71aacece0dfa576d64d3238c2
Mikhail Samin <i@mishasamin.xyz>
2023-08-01 19:48:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,49 @@
+import logging
 from Coach import Coach
 from quoridor.QuoridorGame import QuoridorGame as Game
 from quoridor.pytorch.NNet import NNetWrapper as nn
 from utils import *
+import ray
+ray.init()
 
-args = dotdict({
-    'numIters': 10, #1000
+log = logging.getLogger(__name__)
+
+raw_args = {
+    'numIters': 50, #1000
     'numEps': 50, #100
-    'tempThreshold': 15,
+    'tempThreshold': 25,
     'updateThreshold': 0.55,
     'maxlenOfQueue': 200000,
     'numMCTSSims': 50,
-    'arenaCompare': 40,
+    'arenaCompare': 20,
     'cpuct': 1,
 
     'checkpoint': './temp/',
-    'load_model': True,
-    'load_folder_file': ('./temp','5x5best.pth.tar'),
-    'numItersForTrainExamplesHistory': 20,
-})
+    'load_model': False,
+    'load_folder_file': ('./temp','checkpoint_5.pth.tar'),
+    'numItersForTrainExamplesHistory': 50,
+}
+
+args_ref = ray.put(raw_args)
 
 if __name__=="__main__":
+    args = dotdict(raw_args)
+    log.info('Loading %s...', Game.__name__)
     g = Game(5)
+    print(g.getBoardSize())
+    log.info('Loading %s...', nn.__name__)
     nnet = nn(g)
 
     if args.load_model:
+        log.info('Loading checkpoint "%s/%s"...', args.load_folder_file)
         nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
+    else:
+        log.warning('Not loading a checkpoint!')
 
-    c = Coach(g, nnet, args)
+    log.info('Loading the Coach...')
+    c = Coach(g, nnet, args_ref)
     if args.load_model:
-        print("Load trainExamples from file")
+        log.info("Loading 'trainExamples' from file...")
         c.loadTrainExamples()
+    log.info('Starting the learning process 🎉')
     c.learn()
