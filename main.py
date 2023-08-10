cadaeab11c11cd183ccb3de28e2831d6b2b4330a
Mikhail Samin <i@mishasamin.xyz>
2023-08-10 15:47:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,30 +4,33 @@ from quoridor.pytorch.NNet import NNetWrapper as nn
 from utils import *
 
 args = dotdict({
-    'numIters': 10, #1000
+    'numIters': 500, #1000
     'numEps': 50, #100
-    'tempThreshold': 15,
+    'tempThreshold': 0.3,
     'updateThreshold': 0.55,
     'maxlenOfQueue': 200000,
-    'numMCTSSims': 50,
-    'arenaCompare': 40,
-    'cpuct': 1,
+    'numMCTSSims': 300,
+    'arenaCompare': 20,
+    'cpuct': 2.4,
 
     'checkpoint': './temp/',
-    'load_model': True,
-    'load_folder_file': ('./temp','5x5best.pth.tar'),
-    'numItersForTrainExamplesHistory': 20,
+    'load_model': False,
+    'load_folder_file': ('./temp','best.pth.tar'),
+    'load_folder_examples_file': ('./temp','checkpoint_34.pth.tar'),
+    'load_examples': False,
+    'numItersForTrainExamplesHistory': 100,
 })
 
 if __name__=="__main__":
     g = Game(5)
+    print(g.getBoardSize())
     nnet = nn(g)
 
     if args.load_model:
         nnet.load_checkpoint(args.load_folder_file[0], args.load_folder_file[1])
 
     c = Coach(g, nnet, args)
-    if args.load_model:
+    if args.load_examples:
         print("Load trainExamples from file")
         c.loadTrainExamples()
     c.learn()
